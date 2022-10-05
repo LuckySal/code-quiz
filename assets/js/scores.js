2c4deb345f67c952d9a6d5785f47ec91b71427c0
Brandon <hillbp2014@gmail.com>
2022-10-05 17:18:24 -0400

    completed functionality for high scores page

--- assets/js/scores.js
+++ assets/js/scores.js
@@ -1,21 +1,55 @@
+class Score {
+    constructor(score, initials) {
+        this.score = score;
+        this.initials = initials;
+    }
+    static compare(a, b) {
+        if (a.score > b.score) {
+            return 1;
+        } else if (a.score < b.score) {
+            return -1;
+        } else {
+            if (a.initials > b.initials) {
+                return 1;
+            } else if (a.initials < b.initials) {
+                return -1;
+            } else {
+                return 0;
+            }
+        }
+    }
+}
+
+var scoresListEl = document.getElementById("scores-list");
+var clearButton = document.getElementById("clear-scores");
+
 function printHighscores() {
-    // either get scores from localstorage or set to empty array
-  
-    // sort highscores by score property in descending order
-  
+    // Check for scores in local storage
+    var scores = JSON.parse(localStorage.getItem("highScores"));
+    if (scores) {
+        scores.sort(Score.compare);
+    } else {
+        scores = [];
+    }
     // loop through scores
-      // create li tag for each high score
-  
-      // display on page
-  }
-  
-  /// FUNCTION TO CLEAR SCORES ///
-  function clearHighscores() {
-    // remove an item from local storage
-    // reload the page
-  }
-  
-  /// CLICK EVENT TO RUN THE CLEAR SCORES FUNCTION ///
-  
-  // run function when page loads
-  printHighscores();
+    scores.forEach((element) => {
+        // create li tag for each high score
+        var item = document.createElement("li");
+        item.textContent = `${element.score} ${element.initials}`;
+        // display on page
+        scoresListEl.appendChild(item);
+    });
+}
+
+/// FUNCTION TO CLEAR SCORES ///
+function clearHighscores() {
+    localStorage.removeItem("highScores");
+    printHighscores();
+}
+
+/// CLICK EVENT TO RUN THE CLEAR SCORES FUNCTION ///
+
+// run function when page loads
+printHighscores();
+
+clearButton.addEventListener("click", clearHighscores);
