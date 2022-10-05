c53bce6a9be6f990ee1b7e0e7df0dbdac8ab0a17
Brandon <hillbp2014@gmail.com>
2022-10-05 16:52:25 -0400

    added a sample question

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,3 +1,11 @@
-var questions = [
-
+const quizQuestions = [
+    {
+        id: 0,
+        question: "What is Javascript?",
+        choice1: "A meme",
+        choice2: "Another Meme",
+        choice3: "wrong answer",
+        choice4: "Correct answer",
+        answer: "Correct answer"
+    }
 ];
