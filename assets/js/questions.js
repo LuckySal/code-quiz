217ec8285d57e4fb2a11e61618216216146d6353
Brandon <hillbp2014@gmail.com>
2022-10-09 15:08:07 -0400

    added real quiz questions

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -1,11 +1,43 @@
+// Some questions "borrowed" from https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS
+
 const quizQuestions = [
     {
-        id: 0,
-        question: "What is Javascript?",
-        choices: ["A meme",
-            "Another Meme",
-            "wrong answer",
-            "Correct answer"],
-        answer: "Correct answer"
-    }
+        question: "Inside which HTML element do we put the JavaScript?",
+        choices: [
+            "<scripting>",
+            "<javascript>",
+            "<script>",
+            "<js>"
+        ],
+        answer: "<script>"
+    },
+    {
+        question: "Where is the correct place to insert a JavaScript?",
+        choices: [
+            "The <head> section",
+            "The <body> section",
+            "Both are correct"
+        ],
+        answer: "The <body> section"
+    },
+    {
+        question: "What is the correct syntax for referring to an external script called \"xxx.js\"?",
+        choices: [
+            "<script name=\"xxx.js\">",
+            "<script href=\"xxx.js\">",
+            "<script src=\"xxx.js\">"
+        ],
+        answer: "<script src=\"xxx.js\">"
+    },
+    {
+        question: "Which of the following is NOT a primitive type in JavaScript?",
+        choices: [
+            "boolean",
+            "string",
+            "function",
+            "number"
+        ],
+        answer: "function"
+    },
+    
 ];
